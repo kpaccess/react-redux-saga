a32ef257b71b1dde57e51b03ab25ce6cd21955f2
krishnapradhan <kpaccess@gmail.com>
2020-10-26 17:24:58 -0400

    styling, removed bootstrap

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,9 +4,9 @@ const Card = ({user}) => {
     return (
         <div className="card">
             <div className="card-body">
-                <h5 className="card-title">{user.name}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">{user.company.name}</h6>
-                <p className="card-text">{user.company.catchPhrase}</p>
+                <h5>{user.name}</h5>
+                <h6 className="card-color">{user.company.name}</h6>
+                <p>{user.company.catchPhrase}</p>
             </div>
         </div>
     );
